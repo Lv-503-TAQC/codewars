9c9de5d5da6d812adda4a0160a0f4697596f50e9
Taras Malynovskyi <tarasmslynovskyy@gmail.com>
2020-05-01 22:44:19 +0300

    Add structure of project
    Created kata interfaces and implementations without solutions.

---
+++ src/ss/ita/kata/KataSeven.java
@@ -0,0 +1,12 @@
+package ss.ita.kata;
+
+public interface KataSeven {
+//    Looking for a benefactor
+    long newAvg(final double[] arr, final double navg);
+
+//    Sum of the first nth term of Series
+    String seriesSum(final int n);
+
+//    Where is Vasya?
+    int whereIsHe(final int p, final int bef, final int aft);
+}
