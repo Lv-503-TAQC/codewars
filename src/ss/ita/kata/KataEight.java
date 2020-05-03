e368f392394e7abd39876ab852fc3e5c423c3ccc
Nik <volsknik@gmail.com>
2020-05-03 16:20:38 +0300

    Refresh

---
+++ src/ss/ita/kata/KataEight.java
@@ -0,0 +1,33 @@
+package ss.ita.kata;
+
+public interface KataEight {
+//    Keep Hydrated!
+    int Liters(final double time);
+
+//    Volume of a cuboid
+    double getVolumeOfCuboid(final double length, final double width, final double height);
+
+//    Miles per gallon to kilometers per liter
+    float mpgToKPM(final float mpg);
+
+//    My head is at the wrong end
+    String[] fixTheMeerkat(final String[] arr);
+
+//    To square root or no to square
+    int[] squareOrSquareRoot(final int[] array);
+
+//    Count of positives / sum of negatives
+    int[] countPositivesSumNegatives(final int[] input);
+
+//    Convert a String to a Number!
+    int stringToNumber(final String str);
+
+//    Willson primes
+    boolean amIWilson(final double n);
+
+//    Formatting decimal places
+    double TwoDecimalPlaces(final double number);
+
+//    Find numbers which are divisible by given number
+    int[] divisibleBy(final int[] numbers, final int divider);
+}
---
+++ src/ss/ita/kataimpl/LiubaKataFiveImpl.java
@@ -0,0 +1,37 @@
+package ss.ita.kataimpl;
+
+import ss.ita.kata.KataFive;
+
+import java.math.BigInteger;
+
+public class LiubaKataFiveImpl implements KataFive {
+    @Override
+    public int artificialRain(int[] v) {
+        return 0;
+    }
+
+    @Override
+    public long[] gap(int g, long m, long n) {
+        return new long[0];
+    }
+
+    @Override
+    public int zeros(int n) {
+        return 0;
+    }
+
+    @Override
+    public BigInteger perimeter(BigInteger n) {
+        return null;
+    }
+
+    @Override
+    public double solve(double m) {
+        return 0;
+    }
+
+    @Override
+    public long[] smallest(long n) {
+        return new long[0];
+    }
+}
