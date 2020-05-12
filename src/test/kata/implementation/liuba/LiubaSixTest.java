b1320bd3e9b62e1fd75e00fc6f297e5418ec9e33
ilchenkoliuba <ilchenkoliuba@ukr.net>
2020-05-12 23:04:34 +0300

    Tests for kata six reordered.

--- src/test/kata/implementation/liuba/LiubaSixTest.java
+++ src/test/kata/implementation/liuba/LiubaSixTest.java
@@ -1,29 +1,28 @@
 package test.kata.implementation.liuba;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ss.ita.kata.Six;
 import ss.ita.kata.implementation.liuba.LiubaSix;
-import ss.ita.kata.implementation.mykola.MykolaSix;
-import ss.ita.kata.implementation.nik.NikSix;
-import ss.ita.kata.implementation.taras.TarasSix;
+
 import static org.testng.Assert.assertEquals;
 
-public class BalanceTest {
+public class LiubaSixTest {
 
     Six ipml = new LiubaSix();
 //    Six ipml = new TarasSix();
 //    Six ipml = new NikSix();
 //    Six ipml = new MykolaSix();
 
-    @Test(dataProvider="getData")
-    public void test(String input, String expected) {
+    @Test(dataProvider="balance")
+    public void balance (String input, String expected) {
         String actual = ipml.balance(input);
         assertEquals(actual, expected);
     }
 
     @DataProvider
-    public Object[][] getData() {
+    public Object[][] balance() {
 
         Object [][] data = new Object [2][2];
 
@@ -37,5 +36,45 @@ public class BalanceTest {
 
         return data;
     }
-}
 
+    @Test(dataProvider="findNb")
+    public void findNb (long expected, long input) {
+        long actual = ipml.findNb(input);
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @DataProvider
+    public Object[][] findNb() {
+
+        Object [][] data = new Object [2][2];
+
+        data[0][0] = 2022;
+        data[0][1] = 4183059834009L;
+        data[1][0] = -1;
+        data[1][1] = 24723578342962L;
+
+        return data;
+    }
+
+    @Test(dataProvider="stockSummary")
+    public void stockSummary(String[] codes,String[] letters, String expected) {
+        String actual = ipml.stockSummary(codes, letters);
+        assertEquals(actual, expected);
+    }
+
+    @DataProvider
+    public Object[][] stockSummary() {
+
+        Object [][] data = new Object [2][3];
+
+        data[0][0] = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        data[0][1] = new String[]{"A", "B"};
+        data[0][2] = "(A : 200) - (B : 1140)";
+        data[1][0] = new String[]{"ABAR 200", "CDXE 500", "AJKWR 150", "KTSQ 890", "DRTY 600"};;
+        data[1][1] = new String[]{"A", "B", "C"};
+        data[1][2] = "(A : 350) - (B : 0) - (C : 500)";
+
+        return data;
+    }
+}
